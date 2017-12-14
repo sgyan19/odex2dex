26b28551abde92067cdd9fbb1132bec15f7d9815
dongdongzhang <dongdongzhang@ecarx.com.cn>
2017-12-14 15:06:39 +0800

    feat:init
    Signed-off-by: dongdongzhang <dongdongzhang@ecarx.com.cn>

---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Project: odex2dex
+ * Created by lucky on 2017/12/14.
+ */
+public class main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello word");
+    }
+}
